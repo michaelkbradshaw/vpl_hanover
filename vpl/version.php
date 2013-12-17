c6e0a590a0b2b7119d8f3eaa72812cd33add1aa6
Michael K. Bradshaw <bradshaw@hanover.edu>
2013-12-17 09:50:26 -0500

    put in all of vpl code

---
+++ vpl/version.php
@@ -0,0 +1,13 @@
+<?php // $Id: version.php,v 1.22 2013-07-11 16:47:04 juanca Exp $
+
+defined('MOODLE_INTERNAL') || die();
+
+//$module->version = 2013071112;	//Current module version 2.0.3
+$module->version = 2013071116;	//Current module version 2.0.3 - with my update
+$module->cron    = 300; 		//cron check this module every 5 minutes
+$module->requires = 2012062500;
+$module->maturity = MATURITY_STABLE;
+$module->release = '2.0.3.1';
+$module->component = 'mod_vpl';
+
+?>
